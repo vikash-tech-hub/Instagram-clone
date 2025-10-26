7c3e5e1b78475c0937598b6e0e1e9a1aa1d72646
Vikash kumar <vtech076@gmail.com>
2025-10-26 20:27:13 +0530

    created the post section

---
+++ frontend/src/component/CommentDialog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CommentDialog = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CommentDialog
--- frontend/src/component/Post.jsx
+++ frontend/src/component/Post.jsx
@@ -1,8 +1,11 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
-import { MoreHorizontal } from 'lucide-react'
+import { Bookmark, MessageCircle, MoreHorizontal, Send } from 'lucide-react'
 import React from 'react'
+import { FaRegHeart } from "react-icons/fa";
+import CommentDialog from './CommentDialog'
+
 
 const Post = () => {
     return (
@@ -33,12 +36,36 @@ const Post = () => {
 
             </div>
             <img className='rounded-sm my-2 w-full aspect-square object-cover'
-             src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170"
-              alt="post image" />
+                src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170"
+                alt="post image" />
 
             <div className=''>
-                
+                <div className='flex items-center justify-between my-2'>
+                    <div className='flex items-center gap-3'>
+                        <FaRegHeart size={'22px'} className='cursor-pointer hover:text-gray-600' />
+                        <MessageCircle className='cursor-pointer hover:text-gray-600' />
+                        <Send className='cursor-pointer hover:text-gray-600' />
+                    </div>
+                    <Bookmark className='cursor-pointer hover:text-gray-600' />
+                </div>
 
+
+
+                
+            </div>
+            <span className='font-medium block mb-2'>1k likes</span>
+            <p>
+                <span className='font-medium mr-2'>username</span>
+                caption
+            </p>
+            <span>view all comments</span>
+            <CommentDialog/>
+            <div>
+                <input
+                type='text'
+                placeholder='Add a comment'
+                className='outline-none text-sm w-full'/>
+                <span className='text-[#3BADF8]'>Post</span>
             </div>
 
         </div>

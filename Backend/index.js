cf7edc0ec741308287ba6dff2cda1a1a3255a4df
Vikash kumar <vtech076@gmail.com>
2025-11-17 16:33:40 +0530

    ready for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -8,8 +8,10 @@ import postRoute from "./routes/post.routes.js";
 import messageRoute from "./routes/message.routes.js";
 
 import { app, server } from "./Socket/Socket.js";
+import path from "path"
 
 dotenv.config();
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -28,6 +30,10 @@ app.use(
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
+app.use(express.static(path.join(__dirname,'/frontend/dist')))
+app.get("*",(req,res)=>{
+  res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+})
 
 // start server + socket
 server.listen(PORT, () => {
